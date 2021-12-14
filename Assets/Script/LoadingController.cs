132acffc88db88cc02f9ecb1fbf3d2cfed4837f3
PlzCarryMe <riccowiguna1@gmail.com>
2021-12-14 21:35:44 +0700

    Initial commit

---
+++ Assets/Script/LoadingController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadingController : MonoBehaviour
+{
+    [SerializeField] private Button _localButton;
+    [SerializeField] private Button _cloudButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _localButton.onClick.AddListener(() =>
+        {
+            SetButtonInteractable(false);
+            UserDataManager.LoadFromLocal();
+            SceneManager.LoadScene(1);
+
+        });
+
+        _cloudButton.onClick.AddListener(() =>
+        {
+            SetButtonInteractable(false);
+            StartCoroutine(UserDataManager.LoadFromCloud(() => SceneManager.LoadScene(1)));
+
+        });
+
+    }
+
+    private void SetButtonInteractable(bool interactable)
+    {
+        _localButton.interactable = interactable;
+        _cloudButton.interactable = interactable;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
---
+++ Assets/Script/UserProgressData.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class UserProgressData
+{
+    public double Gold = 0;
+    public List<int> ResourcesLevels = new List<int>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
